dc7b4970015a72e389f3c65b4358aade9bc0a3d6
luoguoling <luoguoling1@gmail.com>
2014-02-15 15:25:41 +0800

    Update chkport.py

--- chkport.py
+++ chkport.py
@@ -15,3 +15,26 @@ def chkport(address,port):
 	except socket.error,e:
 		print "can not conect"
 chkport(options.address,options.port)
+
+#参数的一般使用
+#/usr/bin/python
+import optparse
+from optparse import OptionParser
+def parse_args():
+	usage = """ more arguments must be given"""
+#	parser = optparse.OptionParser(usage)
+	parser = OptionParser(usage)
+	help = "the port to listen"
+	parser.add_option('--port',dest='address',type='int',help=help)
+	help = "the ip address"
+	parser.add_option('--ip',dest='ip',default='localhost',help=help)
+	(options,args) = parser.parse_args()
+#	ip = args[0]
+#	port = args[1]
+	return options
+	#if len(args) != 1:
+#		parser.error('you must offer 1 argument')
+def ta():
+	options = parse_args()
+	print options.ip,options.address
+ta()
