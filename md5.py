a6ef312b21511ce28d9fd178723197a26c938540
luoguoling <luoguoling@gmail.com>
2014-04-04 17:59:36 +0800

    md5
    md5

---
+++ md5.py
@@ -0,0 +1,37 @@
+import os,sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import hashlib,io
+def calMd5(files):
+    m = hashlib.md5()
+    file = io.FileIO(files,'r')
+    bytes = file.read(2048)
+    while(bytes != b''):
+        m.update(bytes)
+        bytes = file.read(1024)
+    file.close()
+    md5value = m.hexdigest()
+    return md5value
+def findDirectory(path):
+        for root,subFolders,files in os.walk(path):
+                if 'weblog' in subFolders:
+                        subFolders.remove('weblog')
+                for filespath in files:
+                        if 'laodao_ynvng_config.php' in files:
+                                files.remove('laodao_ynvng_config.php')
+#                       print os.path.join(root,filespath)
+                        filepath = os.path.join(root,filespath)
+#                       return filepath
+#                       if not os.path.isdir(filepath):
+#			temp = sys.stdout
+#			sys.stdout = open('a.txt','w')
+#			f = open('b.txt','w')
+#			f.truncate()
+#			os.remove('b.txt')
+#			f.close()
+#			sys.stdout = f
+			so = file('b.txt','a+')
+			os.dup2(so.fileno(),sys.stdout.fileno())
+                        print  os.path.join(root,filespath), calMd5(filepath)
+path = '/data/www'
+findDirectory(path)
